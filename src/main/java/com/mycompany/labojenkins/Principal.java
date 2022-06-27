9e6e505a865a5c99c768f20614de14f538fecaf3
MaximilianoZuñiga <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2022-06-27 01:36:06 -0300

    Actualizacion

---
+++ src/main/java/com/mycompany/labojenkins/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.labojenkins;
+
+/**
+ *
+ * @author maxim
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+        Calculadora c1 = new Calculadora();
+        
+        System.out.println(c1.sumar(2, 2));
+        System.out.println(c1.restar(2, 2));
+        System.out.println(c1.multiplicar(2, 2));
+        System.out.println(c1.dividir(2, 2));
+    }
+}
